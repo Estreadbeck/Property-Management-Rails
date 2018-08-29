f8af9edeade11aa3049908f2f62339beedc431e1
Mona Lisa <estreadbeck@estreadbeck-l.corp.instructure.com>
2018-08-29 15:56:25 -0600

    created seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |newsletter|
+  Newsletter.create!(
+      title: "Sample Newsletter #{newsletter}",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ut odio vel ligula laoreet semper. Nullam commodo auctor facilisis. Suspendisse potenti. Integer odio elit, lobortis et iaculis id, sagittis sit amet nulla. Vestibulum commodo, tortor vel scelerisque dapibus, nisl ex bibendum nunc, vel pellentesque nisi sem vel lacus. Proin a orci sed sem fringilla malesuada non eget sem. Nunc vitae tincidunt neque, ut tincidunt nisi. Maecenas sit amet elit sagittis, porttitor quam ac, convallis odio. Duis vel ligula sit amet justo bibendum semper.",
+      image: "https://placeimg.com/350/200/tech"
+    )
+end
+
+puts "created 10 newsletters"
